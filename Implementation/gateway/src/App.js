73e1079e56c7583cf27b929e7a5f4bec18aa751a
hyrum77 <hkm190002@utdallas.edu>
2022-11-12 00:26:36 -0600

    Added sample login page Login.js and testing using Jest.

--- Implementation/gateway/src/App.js
+++ Implementation/gateway/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Login from './Login.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component={null} />
+      <Route exact path='/login' component={Login} />
     </div>
   );
 }
 
-export default App;
+export default Login;
